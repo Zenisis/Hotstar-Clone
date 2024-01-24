5c7d829f4474fb571aad9b7e694260b7b0878cb9
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-01-24 18:58:26 +0530

    Update provider.tf

--- EKS_TERRAFORM/provider.tf
+++ EKS_TERRAFORM/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.30.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "eu-north-1"
 }
