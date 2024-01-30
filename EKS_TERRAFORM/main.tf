210ba1c775fe0627a8a4da9a710056409c88789d
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-01-30 16:15:19 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,3 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+# IAM ROLE FOR EKS CLUSTER
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -90,7 +102,7 @@ resource "aws_eks_node_group" "example" {
     max_size     = 2
     min_size     = 1
   }
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.medium"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
